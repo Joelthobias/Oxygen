3e9bede512378c84ff45752939652a7d0e1c27b1
joel <joelthobias447@gmail.com>
2020-11-25 15:31:36 +0530

    added form validation

--- js/custom.js
+++ js/custom.js
@@ -17,3 +17,28 @@ $(function() {
 })
 
 // When the user scrolls down 50px from the top of the document, resize the header's font size
+
+//when the user scroll down change navbar style
+
+$(function () {
+    var header = $(".navbar-default");
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 155) {
+            header.removeClass('navbar-default nav ').addClass("navbar-s na current");
+        } else {
+            header.removeClass("navbar-s na current").addClass('navbar-default nav ');
+        }
+    });
+});
+
+
+
+
+//form
+
+//form validation
+
+
+
